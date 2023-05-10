bef8e9fc1b6b253bc2957f6c7857cd155fc2eeec
Hitanshu Shah <hitanshushah5@gmail.com>
2023-05-10 23:38:03 +0530

    loading indicator added

--- src/utils/Pages/DisplayFruits.js
+++ src/utils/Pages/DisplayFruits.js
@@ -7,6 +7,7 @@ import Phone from "react-native-phone-number-input";
 import { ValidationCheckData } from "../ValidationCheckdata";
 import { styles } from "./displayfruitstyles";
 import { LinearGradient } from "expo-linear-gradient";
+import { ActivityIndicator } from "react-native";
 import Box from "./Box";
 import Checkout from "./Checkout";
 
@@ -133,6 +134,7 @@ export default function DisplayFruits() {
     axios
       .get("https://fruitmanagement.herokuapp.com/fruit/list")
       .then((response) => {
+        console.log("fruits");
         setFruitName(response.data.fruits[0].name);
         setFruits(response.data.fruits);
         setId(response.data.fruits[0]._id);
@@ -159,71 +161,71 @@ export default function DisplayFruits() {
       end={{ x: 1, y: 1 }}
       colors={["#c2e59c", "#64b3f4"]}
     >
-      <ScrollView>
-        {/* <LinearGradient
-        style={{ flex: 1 }}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        colors={["#c2e59c", "#64b3f4"]}
-      > */}
-        {fruits.map((fruit) => (
-          <>
-            <Text style={styles.header}>{fruit.name}</Text>
-            <View style={styles.subtypecontainer}>
-              {newsubtypes.map((subtype, index) => {
-                return (
-                  <View style={styles.subtypecontainerwithimage} key={index}>
-                    <Image
-                      source={{
-                        uri: `https://fruitmanagement.herokuapp.com/${subtype.imageUrl}`,
-                      }}
-                      style={{ borderRadius: 10 }}
-                      height={150}
-                      width={"100%"}
-                    />
-                    <Box subtype={subtype} />
-                    <TextInput
-                      onChangeText={(val) => (subtype.boxes = val)}
-                      placeholder="Enter quantity"
-                      style={[styles.input, { color: "black" }]}
-                    />
-                  </View>
-                );
-              })}
-            </View>
-          </>
-        ))}
-        <View style={{ alignItems: "center", marginTop: 17 }}>
-          <TextInput
-            onChangeText={(val) => setName(val)}
-            placeholder="Enter Name"
-            value={name}
-            style={styles.input}
-          />
-          <TextInput
-            onChangeText={(val) => setAddress(val)}
-            placeholder="Enter Address"
-            style={styles.input}
-          />
-          <TextInput
-            onChangeText={(val) => setEmail(val)}
-            placeholder="Enter Email"
-            style={styles.input}
-          />
-          <Phone
-            ref={phoneInput}
-            defaultValue={phone}
-            defaultCode={"CY"}
-            layout="first"
-            onChangeFormattedText={(text) => {
-              setPhone(text);
-            }}
-            containerStyle={{ marginTop: 20 }}
-          />
+      {!loadingfruit ? (
+        <ScrollView>
+          {fruits.map((fruit) => (
+            <>
+              <Text style={styles.header}>{fruit.name}</Text>
+              <View style={styles.subtypecontainer}>
+                {newsubtypes.map((subtype, index) => {
+                  return (
+                    <View style={styles.subtypecontainerwithimage} key={index}>
+                      <Image
+                        source={{
+                          uri: `https://fruitmanagement.herokuapp.com/${subtype.imageUrl}`,
+                        }}
+                        style={{ borderRadius: 10 }}
+                        height={150}
+                        width={"100%"}
+                      />
+                      <Box subtype={subtype} />
+                      <TextInput
+                        onChangeText={(val) => (subtype.boxes = val)}
+                        placeholder="Enter quantity"
+                        style={[styles.input, { color: "black" }]}
+                      />
+                    </View>
+                  );
+                })}
+              </View>
+            </>
+          ))}
+          <View style={{ alignItems: "center", marginTop: 17 }}>
+            <TextInput
+              onChangeText={(val) => setName(val)}
+              placeholder="Enter Name"
+              value={name}
+              style={styles.input}
+            />
+            <TextInput
+              onChangeText={(val) => setAddress(val)}
+              placeholder="Enter Address"
+              style={styles.input}
+            />
+            <TextInput
+              onChangeText={(val) => setEmail(val)}
+              placeholder="Enter Email"
+              style={styles.input}
+            />
+            <Phone
+              ref={phoneInput}
+              defaultValue={phone}
+              defaultCode={"CY"}
+              layout="first"
+              onChangeFormattedText={(text) => {
+                setPhone(text);
+              }}
+              containerStyle={{ marginTop: 20 }}
+            />
 
-          <Checkout onClick={() => Order()} />
+            <Checkout onClick={() => Order()} />
+          </View>
+        </ScrollView>
+      ) : (
+        <View style={{ flex: 1, justifyContent: "center", height: 100 }}>
+          <ActivityIndicator />
         </View>
-      </ScrollView>
+      )}
     </LinearGradient>
   );
 }
